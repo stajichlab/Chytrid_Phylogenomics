4a64400af7388b86d2d2f9e8640dca9c41afd125
Jason Stajich <jasonstajich.phd@gmail.com>
2020-04-15 20:02:24 -0700

    query for biosample, locus tag, bioproject

---
+++ LCG/Annotation/scripts/query_biosample_prefix.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import csv, re, sys, os
+import xml.etree.ElementTree as ET
+from Bio import Entrez
+Entrez.email = 'jason.stajich@ucr.edu'
+insamples = "../Assembly/ploidy_target_assembly.tsv"
+outsamples="samples.csv"
+
+if len(sys.argv) > 1:
+    insamples = sys.argv[1]
+
+if len(sys.argv) > 2:
+    outsamples = sys.argv[2]
+
+seen = {}
+if os.path.exists(outsamples):
+    with open(outsamples,"rU") as preprocess:
+        incsv = csv.reader(preprocess,delimiter=",")
+        h = next(incsv)
+        for row in incsv:
+            seen[row[0]] = row
+
+with open(insamples,"rU") as infh, open(outsamples,"w") as outfh:
+    outcsv    = csv.writer(outfh,delimiter=",")
+    outcsv.writerow(['SPECIES','STRAIN','PHYLUM',
+                     'BIOSAMPLE','BIOPROJECT','SRA','LOCUSTAG'])
+
+    samplescsv = csv.reader(infh,delimiter="\t")
+    header = next(samplescsv)
+    for row in samplescsv:
+        print(row)
+        outrow = ["%s %s"%(row[1],row[2]),row[0],row[4]]
+        strain = row[1]
+        if strain in seen:
+            outrow = seen[strain]
+            outcsv.writerow(outrow)
+            continue
+
+        handle = Entrez.esearch(db="biosample",retmax=10,term=strain)
+        record = Entrez.read(handle)
+        handle.close()
+        SRA = ""
+        BIOSAMPLE = ""
+        BIOPROJECT = ""
+        LOCUSTAG = ""
+        BIOPROJECTID=""
+
+        for biosampleid in record["IdList"]:
+            handle = Entrez.efetch(db="biosample", id=biosampleid)
+            tree = ET.parse(handle)
+            root = tree.getroot()
+            for sample in root:
+                BIOSAMPLE = sample.attrib['accession']
+                for ids in root.iter('Ids'):
+                    for id in ids.iter('Id'):
+                        if 'db' in id.attrib and id.attrib['db'] == "SRA":
+                            SRA = id.text
+                for links in root.iter('Links'):
+                    for link in links:
+                        linkdat = link.attrib
+                        if linkdat['type'] == 'entrez':
+                            BIOPROJECT = linkdat['label']
+                            BIOPROJECTID = link.text
+        if BIOPROJECTID:
+            bioproject_handle = Entrez.efetch(db="bioproject",id = BIOPROJECTID)
+            projtree = ET.parse(bioproject_handle)
+            projroot = projtree.getroot()
+
+            lt = projroot.iter('LocusTagPrefix')
+            for locus in lt:
+                LOCUSTAG = locus.text
+        outrow.extend([BIOSAMPLE,BIOPROJECT,SRA,LOCUSTAG])
+        outcsv.writerow(outrow)
