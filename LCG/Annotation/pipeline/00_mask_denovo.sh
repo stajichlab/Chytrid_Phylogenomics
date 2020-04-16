65570e737a9821af530adf4f47ba26d9c9440e1c
Jason Stajich <jasonstajich.phd@gmail.com>
2020-04-15 20:02:02 -0700

    repeat masker run

--- LCG/Annotation/pipeline/00_mask_denovo.sh
+++ LCG/Annotation/pipeline/00_mask_denovo.sh
@@ -24,20 +24,21 @@ fi
 MAX=$(wc -l $SAMPFILE | awk '{print $1}')
 if [ $N -gt $(expr $MAX) ]; then
     MAXSMALL=$(expr $MAX)
-    echo "$N is too big, only $MAXSMALL lines in $SAMPFILE" 
+    echo "$N is too big, only $MAXSMALL lines in $SAMPFILE"
     exit
 fi
 
 IFS=,
-tail -n +2 $SAMPFILE | sed -n ${N}p | while read BASE SPECIES STRAIN BIOPROJECT BIOSAMPLE SRA LOCUS
+tail -n +2 $SAMPFILE | sed -n ${N}p | while read SPECIES STRAIN PHYLUM BIOPROJECT BIOSAMPLE SRA LOCUS
 do
-  name=$BASE
- if [ ! -f $INDIR/${name}.sorted.fasta ]; then
+  name=$(echo -n "$SPECIES" | perl -p -e 's/\s+/_/g')"_$STRAIN"
+  if [ ! -f $INDIR/${name}.sorted.fasta ]; then
      echo "Cannot find $name in $INDIR - may not have been run yet"
      exit
- fi
- 
- if [ ! -f $OUTDIR/${name}.masked.fasta ]; then
+  fi
+  echo "$name"
+  
+  if [ ! -f $OUTDIR/${name}.masked.fasta ]; then
      module unload perl
      module unload python
      module unload miniconda2
@@ -46,20 +47,20 @@ do
      source activate funannotate
      export AUGUSTUS_CONFIG_PATH=$(realpath lib/augustus/3.3/config)
      if [ -f repeat_library/${name}.repeatmodeler-library.fasta ]; then
-	 LIBRARY=repeat_library/${name}.repeatmodeler-library.fasta
-    	 LIBRARY=$(realpath $LIBRARY)
+    	  LIBRARY=$(realpath repeat_library/${name}.repeatmodeler-library.fasta)
      fi
      mkdir $name.mask.$$
      pushd $name.mask.$$
      if [ ! -z $LIBRARY ]; then
-    	 funannotate mask --cpus $CPU -i ../$INDIR/${name}.sorted.fasta -o ../$OUTDIR/${name}.masked.fasta -l $LIBRARY --method repeatmodeler~
-     else	
-	 funannotate mask --cpus $CPU -i ../$INDIR/${name}.sorted.fasta -o ../$OUTDIR/${name}.masked.fasta --method repeatmodeler
-	 rsync -av repeatmodeler-library.*.fasta ../repeat_library/${name}.repeatmodeler-library.fasta
+    	 funannotate mask --cpus $CPU -i ../$INDIR/${name}.sorted.fasta -o ../$OUTDIR/${name}.masked.fasta -l $LIBRARY --method repeatmodeler
+     else
+       funannotate mask --cpus $CPU -i ../$INDIR/${name}.sorted.fasta -o ../$OUTDIR/${name}.masked.fasta --method repeatmodeler
+       mv repeatmodeler-library.*.fasta ../repeat_library/${name}.repeatmodeler-library.fasta
+       mv funannotate-mask.log ../logs/masklog_long.$name.log
      fi
      popd
      rmdir $name.mask.$$
- else 
+  else
      echo "Skipping ${name} as masked already"
- fi 
+  fi
 done
