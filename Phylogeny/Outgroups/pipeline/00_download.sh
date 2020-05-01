5422c9d421b9ac0461d7aa033a0a012bd761a28d
Jason Stajich <jasonstajich.phd@gmail.com>
2020-05-01 12:20:38 -0700

    add extension

--- Phylogeny/Outgroups/pipeline/00_download.sh
+++ Phylogeny/Outgroups/pipeline/00_download.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 module unload perl
 OUTDIR=pep
+EXT=aa.fasta
 mkdir -p $OUTDIR
 cat lib/downloads.tsv | while read PREF OUTNAME URL
 do
-  if [ ! -s $OUTDIR/$OUTNAME ]; then
-    curl $URL | pigz -dc | perl scripts/get_longest.pl -p $PREF > $OUTDIR/$OUTNAME
+  if [ ! -s $OUTDIR/$OUTNAME.$EXT ]; then
+    curl $URL | pigz -dc | perl scripts/get_longest.pl -p $PREF > $OUTDIR/$OUTNAME.$EXT
   fi
 done
