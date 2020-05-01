ab114cf1b874fac893a0d4038d4b2f0b0ec50e22
Jason Stajich <jasonstajich.phd@gmail.com>
2020-04-30 17:10:58 -0700

    download rename

--- Phylogeny/Outgroups/pipeline/00_download.sh
+++ Phylogeny/Outgroups/pipeline/00_download.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-
+module unload perl
 OUTDIR=pep
 mkdir -p $OUTDIR
-cat lib/downloads.txt | while read PREF OUTNAME URL
+cat lib/downloads.tsv | while read PREF OUTNAME URL
 do
-  if [ ! -f $OUTDIR/$OUTNAME ]; then
+  if [ ! -s $OUTDIR/$OUTNAME ]; then
     curl $URL | pigz -dc | perl scripts/get_longest.pl -p $PREF > $OUTDIR/$OUTNAME
   fi
 done
