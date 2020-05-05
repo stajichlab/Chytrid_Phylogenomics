332ca5dcf97569c26352ccd132006cbbd024d23c
Jason Stajich <jasonstajich.phd@gmail.com>
2020-05-05 16:08:00 -0700

    summarize marker recovery

--- Phylogeny/scripts/marker_recovery_table.pl
+++ Phylogeny/scripts/marker_recovery_table.pl
@@ -10,22 +10,26 @@ while(<$fh>) {
     my ($p,$l) = split;
     $lookup{$p} = $l;
 }
-my $dir = "search/JGI_1086";
+my @markers = qw(JGI_1086 fungi_odb9 fungi_odb10);
+foreach my $D ( @markers ) {
 
-opendir(DIR,$dir) || die $!;
-foreach my $file ( readdir(DIR) ) {
-    next unless $file =~ /(\S+)\.best/;
-    my $stem = $1;
-    open(my $fh => "$dir/$file") || die $!;
-    my $n = 0;
-    while(<$fh>) {
-# later lets capture the matrix of markers that are retrieved
-	$n++;
-    }
+    my $dir = "search/$D";
+
+    opendir(DIR,$dir) || die $!;
+    foreach my $file ( readdir(DIR) ) {
+	     next unless $file =~ /(\S+)\.best/;
+	     my $stem = $1;
+	     open(my $fh => "$dir/$file") || die $!;
+    	 my $n = 0;
+	     while(<$fh>) {
+         # later lets capture the matrix of markers that are retrieved
+	        $n++;
+	       }
 #    warn " $stem \n";
-    $table{$lookup{$stem} || $stem} = $n
-}
-print join("\t", qw(SPECIES PHYLING_MARKER_RECOVERY)), "\n";
-foreach my $sp ( sort keys %table ) {
-    print join("\t",($sp, $table{$sp})), "\n";
-}
+	  $table{$lookup{$stem} || $stem}->{$D} = $n;
+    }
+  }
+  print join("\t", qw(SPECIES),@markers), "\n";
+  foreach my $sp ( sort keys %table ) {
+	   print join("\t",($sp, map { $table{$sp}->{$_} || 0 } @markers)), "\n";
+  }
